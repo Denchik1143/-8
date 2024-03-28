eae1fc5e05d443cedafbb2da368668a4d24ad1f3
Denchik1143 <144147611+Denchik1143@users.noreply.github.com>
2024-03-28 11:12:47 +0200

    Create script.js

---
+++ static/script.js
@@ -0,0 +1,20 @@
+// script.js
+function play(userChoice) {
+    var choices = ["rock", "paper", "scissors"];
+    var computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+    var result;
+    if (userChoice === computerChoice) {
+        result = "It's a tie!";
+    } else if (
+        (userChoice === "rock" && computerChoice === "scissors") ||
+        (userChoice === "paper" && computerChoice === "rock") ||
+        (userChoice === "scissors" && computerChoice === "paper")
+    ) {
+        result = "You win!";
+    } else {
+        result = "Computer wins!";
+    }
+
+    document.getElementById("result").innerHTML = "You chose " + userChoice + ". Computer chose " + computerChoice + ". " + result;
+}
